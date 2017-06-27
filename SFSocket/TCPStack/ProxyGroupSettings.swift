920cf924490f13dd6887dd7649c28f28138a7a04
yarshure <yarshure@gmail.com>
2017-06-27 09:49:32 +0800

    add Receipt

--- SFSocket/TCPStack/ProxyGroupSettings.swift
+++ SFSocket/TCPStack/ProxyGroupSettings.swift
@@ -10,6 +10,90 @@ import Foundation
 import SwiftyJSON
 import ObjectMapper
 import AxLogger
+//Verify receipt Success: ["status": 0, "receipt": {
+//"adam_id" = 0;
+//"app_item_id" = 0;
+//"application_version" = 465;
+//"bundle_id" = "com.yarshure.Surf";
+//"download_id" = 0;
+//"in_app" =     (
+//{
+//"is_trial_period" = false;
+//"original_purchase_date" = "2017-06-16 03:16:20 Etc/GMT";
+//"original_purchase_date_ms" = 1497582980000;
+//"original_purchase_date_pst" = "2017-06-15 20:16:20 America/Los_Angeles";
+//"original_transaction_id" = 1000000307670913;
+//"product_id" = "com.yarshure.Surf.Pro";
+//"purchase_date" = "2017-06-16 03:16:20 Etc/GMT";
+//"purchase_date_ms" = 1497582980000;
+//"purchase_date_pst" = "2017-06-15 20:16:20 America/Los_Angeles";
+//quantity = 1;
+//"transaction_id" = 1000000307670913;
+//}
+//);
+//"original_application_version" = "1.0";
+//"original_purchase_date" = "2013-08-01 07:00:00 Etc/GMT";
+//"original_purchase_date_ms" = 1375340400000;
+//"original_purchase_date_pst" = "2013-08-01 00:00:00 America/Los_Angeles";
+//"receipt_creation_date" = "2017-06-16 03:21:00 Etc/GMT";
+//"receipt_creation_date_ms" = 1497583260000;
+//"receipt_creation_date_pst" = "2017-06-15 20:21:00 America/Los_Angeles";
+//"receipt_type" = ProductionSandbox;
+//"request_date" = "2017-06-16 03:43:07 Etc/GMT";
+//"request_date_ms" = 1497584587369;
+//"request_date_pst" = "2017-06-15 20:43:07 America/Los_Angeles";
+//"version_external_identifier" = 0;
+//}, "environment": Sandbox]
+
+
+public class SFItem:CommonModel {
+    public var original_purchase_date:String = ""
+    public var original_purchase_date_ms:String = ""
+    public var product_id:String = ""
+    
+    public override func mapping(map: Map) {
+        original_purchase_date  <- map["original_purchase_date"]
+        original_purchase_date_ms <- map["original_purchase_date_ms"]
+        product_id <- map["product_id"]
+    }
+}
+public class SFStoreReceiptResult: CommonModel {
+    public var status:Int = 0
+    public var receipt:Receipt?
+    public var environment:String = ""
+    public override func mapping(map: Map) {
+        status  <- map["status"]
+        receipt <- map["receipt"]
+        environment <- map["environment"]
+    }
+}
+public class Receipt:CommonModel {
+    public var adam_id:Int = 0
+    public var app_item_id:Int  = 0
+    public var application_version:Int = 0
+    public var bundle_id:String = ""
+    public var download_id:Int = 0
+    public var in_app:[SFItem] = []
+    public var original_application_version:String = ""
+    public var original_purchase_date:String = ""
+    public var original_purchase_date_ms:String = ""
+    
+    public override func mapping(map: Map) {
+        adam_id  <- map["adam_id"]
+        app_item_id <- map["app_item_id"]
+        application_version <- map["application_version"]
+        
+        bundle_id  <- map["bundle_id"]
+        download_id <- map["download_id"]
+        original_application_version <- map["original_application_version"]
+        
+        original_purchase_date  <- map["original_purchase_date"]
+        original_purchase_date_ms <- map["original_purchase_date_ms"]
+        in_app <- map["in_app"]
+        //original_application_version <- map["original_application_version"]
+        
+    }
+}
 public class ProxyGroupSettings:CommonModel {
     public static let share:ProxyGroupSettings = {
         let url = groupContainerURL().appendingPathComponent(kProxyGroupFile)
@@ -49,6 +133,7 @@ public class ProxyGroupSettings:CommonModel {
     public var config:String = "surf.conf"
     public var saveDBIng:Bool = false
     public var lastupData:Date = Date()
+    public var receipt:Receipt?
     public required init?(map: Map) {
         //super.init(map: map)
         super.init()
@@ -92,7 +177,7 @@ public class ProxyGroupSettings:CommonModel {
         wwdcStyle <- map["wwdcStyle"]
         saveDBIng <- map["saveDBIng"]
         lastupData <- (map["lastupData"],self.dateTransform)
-
+        receipt <- map["receipt"]
     }
     
     public func updateStyle(_ s:Bool){
