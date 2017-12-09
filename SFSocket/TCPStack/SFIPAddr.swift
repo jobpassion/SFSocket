2f4bdea840ee0620c3cd87b395dcf17b308e6064
yarshure <support@abigt.net>
2017-12-09 22:35:26 +0800

    update GRDB to v2.4.0

--- SFSocket/TCPStack/SFIPAddr.swift
+++ SFSocket/TCPStack/SFIPAddr.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 
-struct IPAddr {
-    var ip:UInt32 = 0
-    var port:UInt16 = 0
-    init(i:UInt32,p:UInt16){
+public struct IPAddr {
+    public var ip:UInt32 = 0
+    public var port:UInt16 = 0
+    public init(i:UInt32,p:UInt16){
         ip = i
         port = p
     }
@@ -26,11 +26,11 @@ struct IPAddr {
     func p () ->UInt16 {
         return port.byteSwapped
     }
-        mutating func ipFromString(_ str:String) ->UInt32{
+    mutating func ipFromString(_ str:String) ->UInt32{
         ip =  inet_addr(str.cString(using: String.Encoding.utf8)!)
         return ip
     }
-    func equal(_ addr:IPAddr)->Bool{
+    public func equal(_ addr:IPAddr)->Bool{
         if self.ip == addr.ip && self.port == addr.port {
             return true
         }
--- SFSocket/TCPStack/SFIPConnectionInfo.swift
+++ SFSocket/TCPStack/SFIPConnectionInfo.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-struct  SFIPConnectionInfo{
-    var tun:IPAddr
-    var remote:IPAddr
-    init(si:UInt32,sp:UInt16,ri:UInt32,rp:UInt16) {
+public struct  SFIPConnectionInfo{
+    public var tun:IPAddr
+    public var remote:IPAddr
+    public init(si:UInt32,sp:UInt16,ri:UInt32,rp:UInt16) {
         tun = IPAddr.init(i:si, p:sp)
         remote = IPAddr.init(i:ri, p:rp)
     }
-    init(t:IPAddr, r:IPAddr){
+    public init(t:IPAddr, r:IPAddr){
         tun = t
         remote = r
     }
-    func equalInfo(_ info:SFIPConnectionInfo) -> Bool {
+    public func equalInfo(_ info:SFIPConnectionInfo) -> Bool {
         if self.tun.equal(info.tun) && self.remote.equal(info.remote) {
             return true
         }
